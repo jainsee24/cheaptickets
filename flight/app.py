fee97b0b7dd8b0be5cac791e0fa1d9e4ee604bb0
Seemandhar Jain <43344651+jainsee24@users.noreply.github.com>
2024-06-20 15:15:55 -0700

    Update app.py

--- flight/app.py
+++ flight/app.py
@@ -71,4 +71,4 @@ def fetch_flights():
     return jsonify({'flights': flight_data.get('itineraries', [])})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=80)
