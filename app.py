22795496cc29847431be4d3ee2eea9007c71a356
seemandhar jain <sj68@illinois.edu>
2024-07-19 00:57:55 -0700

    ed

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def search_flights():
 def check_flight_status(api_url):
     i=0
     flight_data=''
-    while i<10:
+    while i<20:
         i+=1
         response = requests.get(api_url)
         flight_data1 = response.json()
